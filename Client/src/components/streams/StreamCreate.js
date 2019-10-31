d2b46c814f95dd0cb1608be950270f2cd2b6bd5f
Vinay Sagar Sharma <vinay@r2d2tech.in>
2019-11-01 00:18:42 +0530

    created all action creators and StreamList component is completed

--- Client/src/components/streams/StreamCreate.js
+++ Client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 export class StreamCreate extends Component {
     renderError({ error, touched }){
@@ -30,8 +32,8 @@ export class StreamCreate extends Component {
             // />
         );
     }
-    onSubmit(formValues){
-        console.log(formValues);
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
     }
     render() {
         return (
@@ -58,7 +60,8 @@ const validate = formValues => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped =  reduxForm({
     form: 'streamCreate',
     validate: validate
 })(StreamCreate);
+export default connect(null, { createStream})(formWrapped);
