30269b685b7d8a35034e6e29b8362a10bf37956d
Vinay Sagar Sharma <vinay@r2d2tech.in>
2019-11-01 18:09:45 +0530

    created and modified StreamEdit component

--- Client/src/components/streams/StreamEdit.js
+++ Client/src/components/streams/StreamEdit.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {    
-    return <div>StreamEdit</div>;    
-};
-
-export default StreamEdit;
+class StreamEdit extends React.Component {   
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    render(){
+        if(!this.props.stream){
+            return <div>LOading...</div>;
+        }
+        return <div>{this.props.stream.title}</div>; 
+    }       
+}
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+export default connect(mapStateToProps, { fetchStream})(StreamEdit);
